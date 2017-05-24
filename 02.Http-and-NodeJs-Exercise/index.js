386335f152abb6e76a0a529eedb2d5bb34d8a028
AdrianMladenov <mladenov.adrian@gmail.com>
2017-05-24 03:21:14 +0300

    Http and NodeJs Exercise added

---
+++ 02.Http-and-NodeJs-Exercise/index.js
@@ -0,0 +1,46 @@
+const storage = require('./storage')
+
+console.log('--Testing put--')
+storage.put('zero', '000')
+console.log(storage.get('zero'))
+storage.put('o ye', 2)
+console.log(storage.get('o ye'))
+storage.put('string', false)
+console.log(storage.get('string'))
+storage.put('', null)
+// storage.put('', null)
+console.log(storage.get(''))
+storage.put('undefined', undefined)
+console.log(storage.get('undefined'))
+storage.put('quick brown fox', 'jumps over the lazy dog')
+console.log(storage.get('quick brown fox'))
+console.log()
+
+console.log('--Testing update--')
+storage.update('o ye', 'o ne')
+storage.update('', [])
+console.log(storage.get('o ye'))
+console.log(storage.get(''))
+console.log()
+
+console.log('--Testing delete--')
+storage.delete('undefined')
+console.log(storage.get('undefined'))
+storage.delete('zero')
+console.log(storage.get('zero'))
+console.log()
+
+console.log('--Testing save--')
+storage.save()
+setTimeout(() => {     // testing .clear()
+  storage.get('o ye')  // with 0.100secs delay
+}, 100)                // because throw error breaks .dat file
+console.log()
+
+console.log('--Testing load--')
+storage.load()
+setTimeout(() => {
+  console.log(storage.get('o ye'))
+  console.log(storage.get('quick brown fox'))
+  console.log(storage.get(''))
+}, 100)
---
+++ 02.Http-and-NodeJs-Exercise/storage.js
@@ -0,0 +1,86 @@
+const fs = require('fs')
+const fileName = 'storage.dat'
+
+let globalStorage = {}
+
+let put = (key, value) => {
+  validateString(key)
+  if (globalStorage.hasOwnProperty(key)) {
+    throw new Error('The key already exists')
+  }
+
+  globalStorage[key] = value
+}
+
+let get = (key) => {
+  validateString(key)
+  validateKeyExists(key)
+  return globalStorage[key]
+}
+
+let update = (key, value) => {
+  validateString(key)
+  validateKeyExists(key)
+  globalStorage[key] = value
+}
+
+let deleteItem = (key) => {
+  validateString(key)
+  validateKeyExists(key)
+  globalStorage[key] = undefined
+}
+
+let clear = () => {
+  globalStorage = {}
+}
+
+let save = () => {
+  return new Promise((resolve, reject) => {
+    let storageJSON = JSON.stringify(globalStorage)
+    fs.writeFile(fileName, storageJSON, err => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve()
+    })
+  }).then(clear())
+}
+
+let load = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, 'utf8', (err, dataAsJson) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      globalStorage = JSON.parse(dataAsJson)
+      resolve()
+    })
+  })
+}
+
+module.exports = {
+  put: put,
+  get: get,
+  update: update,
+  delete: deleteItem,
+  clear: clear,
+  save: save,
+  load: load
+}
+
+// helpers
+function validateString (key) {
+  if (typeof key !== 'string') {
+    throw new TypeError('The key must be a string')
+  }
+}
+
+function validateKeyExists (key) {
+  if (!globalStorage.hasOwnProperty(key)) {
+    throw new Error('The key do not exists')
+  }
+}
